41f66bc0a81e1f9bf9317c5293e2f28537a64c2d
Ayushi Raj <152719682+ayushi-raj89@users.noreply.github.com>
2026-06-12 00:52:21 -0700

    fix: add self-destructing service worker to force clear old client caches

---
+++ public/sw.js
@@ -0,0 +1,16 @@
+// Self-destructing service worker to force-clear previous site caches
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    self.registration.unregister()
+      .then(() => self.clients.matchAll())
+      .then((clients) => {
+        clients.forEach((client) => {
+          client.navigate(client.url);
+        });
+      })
+  );
+});
